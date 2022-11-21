8abc5109b75cd94fb5ea125c6706092ac19750cb
SergNifant <n777@bk.ru>
2022-11-21 21:30:48 +0300

    21 11 3

--- auto/backend/main.py
+++ auto/backend/main.py
@@ -1,18 +1,32 @@
-#main.py
-
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
-
-app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
-
-@app.get("/")
-def hello_api():
-    return {"msg":"Hello API"}
-
-#from apis.base import api_router
-#from webapps.base import api_router as web_app_router #new
+from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine   #new
+from db.base import Base  #new
 
 
-#def include_router(app):
-#    app.include_router(api_router)
-#    app.include_router(web_app_router)  #new
+# def create_tables():
+# 	print("create_tables")
+# 	Base.metadata.create_all(bind=engine)
+
+def include_router(app):
+	app.include_router(general_pages_router)
+
+
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def create_tables():           #new
+	Base.metadata.create_all(bind=engine)
+
+	
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+	include_router(app)
+	configure_static(app)
+	create_tables()       #new
+	return app
+
+app = start_application()
