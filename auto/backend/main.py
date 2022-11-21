b80df3b276b5955323e2e4f7743cd7ac027cff74
SergNifant <n777@bk.ru>
2022-11-21 17:45:14 +0300

    commit

--- auto/backend/main.py
+++ auto/backend/main.py
@@ -1,7 +1,18 @@
-from apis.base import api_router
-from webapps.base import api_router as web_app_router #new
+#main.py
+
+from fastapi import FastAPI
+from core.config import settings
+
+app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+
+@app.get("/")
+def hello_api():
+    return {"msg":"Hello API"}
+
+#from apis.base import api_router
+#from webapps.base import api_router as web_app_router #new
 
 
-def include_router(app):
-    app.include_router(api_router)
-    app.include_router(web_app_router)  #new
+#def include_router(app):
+#    app.include_router(api_router)
+#    app.include_router(web_app_router)  #new
