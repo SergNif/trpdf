83f2ff03e9017ff0d87aecbc7859aa95dc8f963f
SergNifant <n777@bk.ru>
2022-11-27 20:51:42 +0300

    22 11 2

--- site/build_tree_folder.py
+++ site/build_tree_folder.py
@@ -47,26 +47,14 @@ def find_nearest_min_key_dir(num_key: int) -> list:
     res = 0
     last_elem_dict = False
     for ix, (key, val) in enumerate(dct_dir.items()):
-        # print(f'{ix=} {len(dct_dir.items())=}')
-        # if ix == len(dct_dir.items())-1:
-        #     last_elem_dict = True
-        #     res = ix
-        #     break
+
         if key > num_key:
             res = ix-1
             break
         res = ix
 
-    # if res > 0:
-    #     res -= 1
-    # else:
-    #     return [0, '']
-
     while (res >= 0) and (dct_file[num_key][0] <= list(dct_dir.items())[res][1][0]):
         res -= 1
-        # print(f'{num_key=} {res=}  {dct_file[num_key]=}')
-        # print(f'{list(dct_dir.items())[res][1]=}')
-        # print(f'dct_file {"" if res<0 else list(dct_dir.items())[res][1]}')
 
     if res < 0:
         result = [[0, [0, '']], res]
@@ -75,29 +63,24 @@ def find_nearest_min_key_dir(num_key: int) -> list:
 
     return result
 
-    # if dct_file[num_key][0] >= list(dct_dir.items())[res][1][0]:
-    #     print(f'{num_key=} {res=}  {dct_file[num_key]=}')
-    #     print(f'{list(dct_dir.items())[res][1]=}')
-    #     print(f'dct_file {"" if res-1<0 else list(dct_dir.items())[res-1][1]}')
-    #     return [0, ''] if res-1<0 else list(dct_dir.items())[res-1][1]
-
-    # return list(dct_dir.items())[res][1]
-
 
 for key, val in dct_file.items():
-    # print(f'onee  {key} {val}')
+
     list_folder = find_nearest_min_key_dir(key)
-    # print(f'{list_folder=}')
 
     name = list_folder[0][1][1]
     if list_folder[0][1][0] > 1:
         order_folder = list_folder[0][1][0]
-        for el in range(list_folder[1],-1,-1):
+        for el in range(list_folder[1], -1, -1):
             if order_folder > list(dct_dir.items())[el][1][0]:
                 name = list(dct_dir.items())[el][1][1] + '/' + name
                 if list(dct_dir.items())[el][1][0] == 1:
                     break
 
+    folder = '' if len(list_folder[0][1][1]) == 0 else name+'/'
+    print(f'{folder}\n{val[1]}')
 
-    folder = '' if len(list_folder[0][1][1]) == 0 else  name+'/' # list_folder[0][1][1]+'/'
-    print(f'{folder}{val[1]}')
+    if len(folder) > 0:
+        make_dir(path=folder)
+
+    create_file(name=folder+val[1])
